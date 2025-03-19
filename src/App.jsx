096f9579a03f2dd6a4f5f07199d2daf4d25aa3e6
Hardik-M-Agarwal <hardikag04@gmail.com>
2025-03-20 01:20:18 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+import {useRef} from 'react';
+
+function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+    }
+
+  return (
+    <div className="App">
+      <div ref={contentRef} id="content">
+      <Navbar/>
+      <Jumbotron/>
+      <SoundSection/>
+      <DisplaySection triggerPreview = {handlePreview}/>
+      </div>
+      <WebgiViewer contentRef={contentRef} ref = {webgiViewerRef}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const DisplaySection = ({triggerPreview}) => {
+
+  const handleScrollToTop = () =>{
+    window.scrollTo({ top:0 , left: 0, behaviour: "smooth"})
+  }
+
+  return (
+    <div className='display-section wrapper' id='display'>
+        <h2 className='title'>New</h2>
+        <p className='text'>Brilliant.</p>
+        <span className='description'>A display that's up to 2x brighter in the sun.</span>
+        <button className='button' onClick={triggerPreview}>Try Me!</button>
+        <button className='back-button' onClick ={handleScrollToTop}>Top</button>
+    </div>
+  )
+}
+
+export default DisplaySection
